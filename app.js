eb2a7d006600e19d223ed631a6bc949bd00e25b9
Robert <roope_larsson@outlook.com>
2022-01-29 21:56:23 +0200

    Main function DONE

--- app.js
+++ app.js
@@ -3,15 +3,54 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-//app.use(express.static('public'));
-app.use(express.static('public'));
+// Laurans values
+var newGameNameItems = [];
+var newGamePlatformItems = [];
+var newGameReleasedateItems = [];
+
+// Roopes values
+var newGameNameItemsR = [];
+var newGamePlatformItemsR = [];
+var newGameReleasedateItemsR = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
-  var today = new Date();
-
-  res.sendFile(__dirname + '/gamepage.html');
+  // var today = new Date();
+  // var options = {
+  //   weekday: 'long',
+  //   day: 'numeric',
+  //   month: 'long'
+  // };
+  // var day = today.toLocaleDateString('en-US', options);
+  res.render('gamelist',
+  {newGameNameItems: newGameNameItems, newGamePlatformItems: newGamePlatformItems, newGameReleasedateItems: newGameReleasedateItems, newGameNameItemsR: newGameNameItemsR, newGamePlatformItemsR: newGamePlatformItemsR, newGameReleasedateItemsR: newGameReleasedateItemsR});
 });
 
-app.listen(process.env.PORT || 3000, function() {
+app.post('/', function(req, res) {
+  user = req.body.user;
+  // Lauras inputs
+  newGameName = req.body.gameName;
+  newGamePlatform = req.body.platform;
+  newGameReleaseDate = req.body.releaseDate;
+
+if (user === 'laura') {
+  // Push Lauras values
+  newGameNameItems.push(newGameName);
+  newGamePlatformItems.push(newGamePlatform);
+  newGameReleasedateItems.push(newGameReleaseDate);
+} else if (user === 'roope') {
+  // Push Roopes values
+  newGameNameItemsR.push(newGameName);
+  newGamePlatformItemsR.push(newGameName);
+  newGameReleasedateItemsR.push(newGameReleaseDate);
+}
+
+  res.redirect('/');
+});
+
+app.listen(3000, function() {
   console.log('Server running on port 3000');
 });

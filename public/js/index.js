5ce519e98cac98a1cb03b25288eabc1734fe99dc
Robert <roope_larsson@outlook.com>
2022-02-11 15:52:53 +0200

    UPDATE: Review form field edit

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-$('#selectTable').change(function() {
+  $('#selectTable').change(function() {
     if ($(this).val() == 'roope' || $(this).val() == 'laura') {
       $('#formGroup').css('display', 'initial');
     } else {
@@ -19,5 +19,15 @@ $('#selectTable').change(function() {
       $('#deleteLabel').css('display', 'none');
       $('#deleteL').css('display', 'none');
       $('#deleteR').css('display', 'none');
+    }
+  });
+
+  $('#releasedGame').change(function() {
+    var gameNameLength = $('#releasedGame').val().length;
+
+    if (gameNameLength > 0) {
+      $('#reviewFields').css('display', 'initial');
+    } else {
+      $('#reviewFields').css('display', 'none');
     }
   });
